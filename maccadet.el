7af3ae4804d2eb6dcbabfe0427e46632a17467a4
Grant Rettke <grant@wisdomandwonder.com>
2019-08-31 18:05:50 -0500

    Make Alt the major key

--- maccadet.el
+++ maccadet.el
@@ -1,7 +1,7 @@
 (setq ns-control-modifier 'control)
-(setq ns-alternate-modifier 'hyper)
+(setq ns-alternate-modifier 'alt)
 (setq ns-command-modifier 'meta)
 
 (setq ns-right-command-modifier 'super)
 (setq ns-right-alternate-modifier 'none)
-(setq ns-right-control-modifier 'alt)
+(setq ns-right-control-modifier 'hyper)

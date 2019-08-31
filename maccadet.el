920faeb3538a8b1e6e9be9bbbe3571fbdb907912
Grant Rettke <grant@wisdomandwonder.com>
2019-08-31 17:59:34 -0500

    Keyorder

--- maccadet.el
+++ maccadet.el
@@ -1,11 +1,7 @@
 (setq ns-control-modifier 'control)
-
-(setq ns-right-control-modifier 'alt)
-
 (setq ns-alternate-modifier 'hyper)
-
-(setq ns-right-alternate-modifier 'none)
-
 (setq ns-command-modifier 'meta)
 
 (setq ns-right-command-modifier 'super)
+(setq ns-right-alternate-modifier 'none)
+(setq ns-right-control-modifier 'alt)

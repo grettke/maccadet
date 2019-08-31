7085a9ccae16fc354d4c8be667625d10d55994e8
Grant Rettke <grant@wisdomandwonder.com>
2019-08-31 17:56:17 -0500

    Import keybindings

---
+++ maccadet.el
@@ -0,0 +1,6 @@
+(setq ns-control-modifier 'control)
+(setq ns-command-modifier 'meta)
+(setq ns-alternate-modifier 'hyper)
+(setq ns-right-control-modifier 'alt)
+(setq ns-right-command-modifier 'super)
+(setq ns-right-alternate-modifier 'none)

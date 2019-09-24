f2c48b4348fa823b7cb12e117c38915a083aa3d6
Grant Rettke <grant@wisdomandwonder.com>
2019-09-24 18:57:06 -0500

    Version 4

--- maccadet.el
+++ maccadet.el
@@ -1,7 +1,15 @@
-(setq ns-control-modifier 'control)
-(setq ns-alternate-modifier 'alt)
-(setq ns-command-modifier 'meta)
+(defalias 'ns-left-control-modifier 'ns-control-modifier)
+(defalias 'ns-left-option-modifier 'ns-option-modifier)
+(defalias 'ns-left-command-modifier 'ns-command-modifier)
+
+(setq ns-left-control-modifier 'control)
+
+(setq ns-right-control-modifier 'meta)
+
+(setq ns-left-alternate-modifier 'hyper)
+
+(setq ns-left-command-modifier 'alt)
 
 (setq ns-right-command-modifier 'super)
+
 (setq ns-right-alternate-modifier 'none)
-(setq ns-right-control-modifier 'hyper)
